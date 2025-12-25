eee3542c5db41fd04a35798ea0063c3159ef2348
Kavia AI <production@kavia.ai>
2025-12-25 16:53:36 +0000

    Checkpoint 8 - cge7337530

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -14,7 +14,7 @@ import './App.css';
  */
 function App() {
   return (
-    <Router>
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AuthProvider>
         <Routes>
           {/* Public routes */}
